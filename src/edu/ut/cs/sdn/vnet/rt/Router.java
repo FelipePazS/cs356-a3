70566f1dac35f47eeda4077732828b5700d403be
Michael Tran <jmtran678@gmail.com>
2022-10-17 15:33:52 -0500

    handle icmp

--- src/edu/ut/cs/sdn/vnet/rt/Router.java
+++ src/edu/ut/cs/sdn/vnet/rt/Router.java
@@ -4,7 +4,9 @@ import edu.ut.cs.sdn.vnet.Device;
 import edu.ut.cs.sdn.vnet.DumpFile;
 import edu.ut.cs.sdn.vnet.Iface;
 
+import net.floodlightcontroller.packet.Data;
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.ICMP;
 import net.floodlightcontroller.packet.IPv4;
 
 /**
@@ -17,6 +19,19 @@ public class Router extends Device
 	
 	/** ARP cache for the router */
 	private ArpCache arpCache;
+
+	private static final int ICMP_TIME_EXCEEDED_TYPE = 11;
+	private static final int ICMP_TIME_EXCEEDED_CODE = 0;
+	private static final int ICMP_DEST_NET_UNREACH_TYPE = 3;
+	private static final int ICMP_DEST_NET_UNREACH_CODE = 0;
+	private static final int ICMP_DEST_HOST_UNREACH_TYPE = 3;
+	private static final int ICMP_DEST_HOST_UNREACH_CODE = 1;
+	private static final int ICMP_DEST_PORT_UNREACH_TYPE = 3;
+	private static final int ICMP_DEST_PORT_UNREACH_CODE = 3;
+	private static final int ICMP_ECHO_REQ = 8;
+
+
+
 	
 	/**
 	 * Creates a router for a specific host.
@@ -119,7 +134,10 @@ public class Router extends Device
         // Check TTL
         ipPacket.setTtl((byte)(ipPacket.getTtl()-1));
         if (0 == ipPacket.getTtl())
-        { return; }
+        {
+        	this.handleICMP(etherPacket, inIface, ICMP_TIME_EXCEEDED_TYPE, ICMP_TIME_EXCEEDED_CODE);
+        	return;
+        }
         
         // Reset checksum now that TTL is decremented
         ipPacket.resetChecksum();
@@ -127,8 +145,18 @@ public class Router extends Device
         // Check if packet is destined for one of router's interfaces
         for (Iface iface : this.interfaces.values())
         {
-        	if (ipPacket.getDestinationAddress() == iface.getIpAddress())
-        	{ return; }
+        	if (ipPacket.getDestinationAddress() == iface.getIpAddress()) {
+        		byte protocol = ipPacket.getProtocol();
+        		if (protocol == IPv4.PROTOCOL_TCP || protocol == IPv4.PROTOCOL_UDP) {
+        			this.handleICMP(etherPacket,inIface, ICMP_DEST_PORT_UNREACH_TYPE, ICMP_DEST_PORT_UNREACH_CODE);
+				} else if (protocol == IPv4.PROTOCOL_ICMP) {
+        			ICMP icmp = (ICMP) ipPacket.getPayload();
+        			if (icmp.getIcmpType() == ICMP.TYPE_ECHO_REQUEST) {
+        				this.handleICMP(etherPacket, inIface, ICMP_ECHO_REQ, 0);
+					}
+				}
+        		return;
+        	}
         }
 		
         // Do route lookup and forward
@@ -151,7 +179,10 @@ public class Router extends Device
 
         // If no entry matched, do nothing
         if (null == bestMatch)
-        { return; }
+        {
+        	this.handleICMP(etherPacket, inIface, ICMP_DEST_NET_UNREACH_TYPE, ICMP_DEST_NET_UNREACH_CODE);
+        	return;
+        }
 
         // Make sure we don't sent a packet back out the interface it came in
         Iface outIface = bestMatch.getInterface();
@@ -174,4 +205,77 @@ public class Router extends Device
         
         this.sendPacket(etherPacket, outIface);
     }
+
+    private void handleICMP(Ethernet ogEtherPacket, Iface ogIface, int IcmpType, int IcmpCode) {
+
+		IPv4 ogIpPacket = (IPv4) ogEtherPacket.getPayload();
+
+		Ethernet ether = new Ethernet();
+		ether.setEtherType(Ethernet.TYPE_IPv4);
+		ether.setSourceMACAddress(ogIface.getMacAddress().toBytes());
+
+		int sourceAddr = ogIpPacket.getSourceAddress();
+
+		RouteEntry bestMatch = this.routeTable.lookup(sourceAddr);
+
+		// If no entry matched, do nothing
+		if (null == bestMatch)
+		{ return; }
+
+		// If no gateway, then nextHop is IP destination
+		int nextHop = bestMatch.getGatewayAddress();
+		if (0 == nextHop)
+		{ nextHop = sourceAddr; }
+
+		// Set destination MAC address in Ethernet header
+		ArpEntry arpEntry = this.arpCache.lookup(nextHop);
+		if (null == arpEntry)
+		{ return; }
+
+		ether.setDestinationMACAddress(arpEntry.getMac().toBytes());
+
+		// Set IP header
+		IPv4 ip = new IPv4();
+		final byte ICMP_STANDARD_TTL = 64;
+		ip.setTtl(ICMP_STANDARD_TTL);
+		ip.setProtocol(IPv4.PROTOCOL_ICMP);
+		if (IcmpType == ICMP_ECHO_REQ) {
+			ip.setSourceAddress(ogIpPacket.getDestinationAddress());
+		} else {
+			ip.setSourceAddress(ogIface.getIpAddress());
+		}
+		ip.setDestinationAddress(ogIpPacket.getSourceAddress());
+
+		// Set ICMP header
+		ICMP icmp = new ICMP();
+		icmp.setIcmpCode((byte) IcmpCode);
+		icmp.setIcmpType((byte) IcmpType);
+
+		// Set Data
+		Data data = new Data();
+		if (IcmpType == ICMP_ECHO_REQ) {
+			ICMP echoPayload = (ICMP) ogIpPacket.getPayload();
+			icmp.setIcmpCode((byte) 0);
+			icmp.setIcmpType((byte) 0);
+			data.setData(echoPayload.getPayload().serialize());
+		} else {
+			int ipHeaderLength = ogIpPacket.getHeaderLength() * 4;
+			final int NUM_BYTES_AFTER_IP_HEADER = 8;
+			final int NUM_BYTES_PADDING = 4;
+			int dataPayloadLength = NUM_BYTES_PADDING + ipHeaderLength + NUM_BYTES_AFTER_IP_HEADER;
+			byte[] dataPayload = new byte[dataPayloadLength];
+			byte[] ogData = ogIpPacket.serialize();
+			for (int i = 0; i < ipHeaderLength + NUM_BYTES_AFTER_IP_HEADER; i++) {
+				dataPayload[i + NUM_BYTES_PADDING] = ogData[i];
+			}
+			data.setData(dataPayload);
+		}
+
+		ether.setPayload(ip);
+		ip.setPayload(icmp);
+		icmp.setPayload(data);
+		this.sendPacket(ether, ogIface);
+	}
+
+
 }
